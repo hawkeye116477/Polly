16119eef126ca773828e388f9d0a16447b48da1a
zakius@windowslive.com <jaroslaw.jarosik@outlook.com>
2020-11-03 09:31:25 +0100

    Add Polyfill.io bundles Add BigInt polyfill

---
+++ polyfills/Polyfill.io/globalThis.js
@@ -0,0 +1,3 @@
+/* Disable minification (remove `.min` from URL path) for more info */
+
+(function(self, undefined) {!function(){function e(e){return e&&e.Object==Object&&e}var t=e("object"==typeof c&&c),o=e("object"==typeof window&&window),b=e("object"==typeof self&&self),l=e("object"==typeof global&&global),c=t||o||b||l||Function("return this")();try{Object.defineProperty(c,"globalThis",{configurable:!0,enumerable:!1,writable:!0,value:c})}catch(n){c.globalThis=c}}();})('object' === typeof window && window || 'object' === typeof self && self || 'object' === typeof global && global || {});
